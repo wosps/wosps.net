7cd5290a7fdd18cae22496506f8b6d61ee95441c
Jord <7325535+wosps@users.noreply.github.com>
2023-02-07 21:05:13 +0000

    💄 added project images and work page
    Updated Project component to hold a cover image to display next to the project overview.
    
    next/link used to link new 'Work' page.

--- components/LatestWork.js
+++ components/LatestWork.js
@@ -1,20 +1,16 @@
-import Image from 'next/image'
-import Link from 'next/link'
 import Project from './Project'
-import { TbArrowRight } from "react-icons/tb";
 import styles from '../styles/SelectedWork.module.css'
 
-export default function SelectedWork() {
+export default function LatestWork() {
   return (
     <section>
       <div className={styles.header}>
-        <h2 className={styles.heading}>Selected Work</h2>
-        <Link className={styles.flex+ ' ' + styles.link} href='/work'><h3>View More</h3>
-          <TbArrowRight className={styles.icon} /></Link>
+        <h2 className={styles.heading}>Latest Work</h2>
       </div>
         <Project title="VinterMap" summary="An interactive web map built using Leaflet.js for the video-game ‘Longvinter’." img="/vintermap.png" />
         <Project title="Tagger for Letterboxd" summary="A browser extension that adds further functionality to ‘Letterboxd’ via my backend server and the tMDB API." img="/tagger.png" />
         <Project title="Colour Picker" summary="An interactive colour picker built using vanilla JavaScript and TheColorAPI." img="/colour.png" />
+        <Project title="Example Project" summary="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dolor orci, euismod in ante at, volutpat tincidunt risus. " img="/stock.jpg" />
     </section>
   )
 }
--- components/Navigation.js
+++ components/Navigation.js
@@ -1,15 +1,16 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Navigation.module.css'
 
 export default function Navigation() {
   return (
     <nav className={styles.container}>
       <div>
-        <Image src="/logo.png" min-width="40" width="40" height="40" alt='Logo'/>
+        <Link href='/'><Image src="/logo.png" min-width="30" width="30" height="30" alt='Logo'/></Link>
       </div>
       <div className={styles.navlinks}>
         <h2>Profile</h2>
-        <h2>Work</h2>
+        <Link className={styles.link} href="/work"><h2>Work</h2></Link>
         <h2>Contact</h2>
       </div>
     </nav>
